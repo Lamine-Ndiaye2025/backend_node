142f13301876eaa54717417db4aef1c21054ccae
lamine ndiaye <laminendiaye0444@gmail.com>
2026-06-22 15:36:36 +0000

    iii

--- models/QuestionModel.js
+++ models/QuestionModel.js
@@ -19,6 +19,48 @@ const questionSchema = new mongoose.Schema(
       ref: "User",
       required: true,
     },
+
+    // Nombre de vues
+    vues: {
+      type: Number,
+      default: 0,
+    },
+
+    // Votes positifs
+    upVotes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    // Votes négatifs
+    downVotes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    // Nombre de réponses
+    nombreReponses: {
+      type: Number,
+      default: 0,
+    },
+
+    // Tags de la question
+    tags: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+
+    // Question résolue ou non
+    resolue: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     timestamps: true,
